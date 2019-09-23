ca4d0da2c521fd0879b28497896e1f6a28805813
Michael Tran <Michael.Tran@sandstone.com.au>
2019-09-23 22:44:20 +1000

    Added some parsing for SassList, SassBoolean and SassNull data types for converting them to JSON format.

--- index.js
+++ index.js
@@ -9,14 +9,21 @@ class SassVarsToJSON {
       case 'SassMap':
         return SassVarsToJSON.parseChunks(chunk.value)
 
+      case 'SassList':
+        return SassVarsToJSON.parseList(chunk)
+
       case 'SassColor':
-        return chunk.value.hex
+        return SassVarsToJSON.parseColor(chunk.value)
 
       case 'SassString':
         return chunk.value
 
       case 'SassNumber':
         return chunk.value + chunk.unit
+
+      case 'SassBoolean':
+      case 'SassNull':
+        return chunk.value
     }
   }
 
@@ -24,6 +31,27 @@ class SassVarsToJSON {
     return _.mapValues(chunks, (chunk, name) => {
       return SassVarsToJSON.parseTypeToValue(chunk)
     })
+  }
+
+  static parseColor (color) {
+    if (color.a < 1) {
+      return `rgba(${color.r}, ${color.g}, ${color.b}, ${color.a})`
+    }
+
+    return color.hex
+  }
+
+  static parseList (list) {
+    const values = list.value
+    let parsedString = ''
+
+    const separator = list.separator !== ' ' ? list.separator + ' ' : list.separator;
+
+    values.forEach((value, index) => {
+      parsedString += `${SassVarsToJSON.parseTypeToValue(value)}${index === values.length - 1 ? '' : separator}`
+    })
+
+    return parsedString
   }
 
   static extract (file) {
